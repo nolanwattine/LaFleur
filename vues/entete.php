fee322d878b4480da5847909b7eaf7f3671faa01
nolanwattine <nolanwattine@gmail.com>
2025-09-09 09:30:12 +0200

    modif de con

--- vues/entete.php
+++ vues/entete.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="iso8859-2">
-    <title>Lafleur</title>
+    <title>Lafleur de con</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
